77b8cdac5d3e67471eec75da24a17a6f85bc238a
ianglover <gloveri@newschool.edu>
2022-03-11 11:15:12 -0500

    Update In-Class Workshop

--- demonstrations/demonstration-3/index.js
+++ demonstrations/demonstration-3/index.js
@@ -1,11 +1,3 @@
-// Loops 
-
-const names = ["Name One", "Ian", "Name Three", "Name Four", "name Five"]
-
-const isUserIan = function(input) {
-    return input === "Ian"
-}
-
-for (let index = 0; index < names.length; index++) {
-  console.log(isUserIan(names[index]))
-}
+const newInterval = setInterval(function() {
+    console.log(new Date().getMinutes())
+}, 1000)
