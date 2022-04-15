a932ad414f7cc15122da145f1faac604e214cd7e
Ian Glover <ianglover@ianglover.com>
2022-04-15 07:56:00 -0400

    Google Drive Demonstration

---
+++ demonstrations/demonstration-7/index.js
@@ -0,0 +1,66 @@
+// STEP 1: VARIABLES
+// Our API Key for Airtable
+const airtableApiKey = "keyUdTIA7XPdyI7Vi";
+
+// URL of Where our Data is Located
+const googleDriveTableUrl =
+  "https://api.airtable.com/v0/appNBOMaj7ch0wKcM/Google-Drive-Illustrations";
+const tableUrl = "https://api.airtable.com/v0/appNBOMaj7ch0wKcM/Illustrations";
+
+// URL with API Key Authentication
+const googleDriveAuthenticatedUrl =
+  googleDriveTableUrl + "?api_key=" + airtableApiKey;
+const authenticatedUrl = tableUrl + "?api_key=" + airtableApiKey;
+
+// STEP 2: DOM REFERENCES
+const bodyElement = document.querySelector("main#app");
+
+// STEP 3: USING FUNCTIONS, VARIABLES, AND LOOPS
+
+// Get a 'Promise' Representing the Data that Will be Returned from the Google Drive URL
+fetch(googleDriveAuthenticatedUrl)
+  // When 'Promise' is 'Resolved', get the JSON data
+  .then((res) => res.json())
+  // Use the JSON Data
+  .then((data) => {
+    console.log("Google Drive Table Data: ", data.records);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Get a 'Promise' Representing the Data that Will be Returned
+fetch(authenticatedUrl)
+  // When 'Promise' is 'Resolved', get the JSON data
+  .then((res) => res.json())
+  // Use the JSON Data
+  .then((data) => {
+    const illustrations = data.records;
+    console.log("Illustration Table Data: ", illustrations);
+    for (let index = 0; index < illustrations.length; index++) {
+      // Get Illustration
+      const illustration = illustrations[index];
+      //   Get Title
+      const title = illustration.fields.Title;
+      // Get Image Url
+      const imageUrl = illustration.fields.Image[0].url;
+      // Create Container Object
+      const containerElement = document.createElement("div");
+      containerElement.classList.add("container");
+      // Create Image Element
+      const imageElement = document.createElement("img");
+      imageElement.classList.add("image");
+      imageElement.setAttribute("src", imageUrl);
+      // Create Title Element
+      const titleElement = document.createElement("p");
+      titleElement.classList.add("title");
+      titleElement.innerHTML = title;
+      // Add Elements to DOM
+      bodyElement.appendChild(containerElement);
+      containerElement.appendChild(imageElement);
+      containerElement.appendChild(titleElement);
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
